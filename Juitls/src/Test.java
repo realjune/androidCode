50cddba533be9d02e4b2965f25e35c329a46a1d3
Junxu Wang <wangjunxu@yuandian.cn>
2015-03-20 19:23:02 +0800

    update

---
+++ Juitls/src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+	public static void main(String[]args){
+		System.out.println("5+0xffffffff="+((~-5)));
+		System.out.println("5+0xffffffff="+((byte)0xff));
+		
+		
+	}
+}
--- wu_template/src/wu/a/template/MainActivity.java
+++ wu_template/src/wu/a/template/MainActivity.java
@@ -1,15 +1,74 @@
 package wu.a.template;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import wu.a.template.app.AppListActivity;
+import wu.a.template.media.MediaAcitivity;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setListAdapter(
+	        new SimpleAdapter(
+	            this, getData(), android.R.layout.simple_list_item_1, new String[]{"title"},
+	            new int[]{android.R.id.text1}
+	        )
+	    );
+	    getListView().setScrollbarFadingEnabled(false);
+	  }
+
+	  @Override
+	  public boolean onCreateOptionsMenu(Menu menu) {
+	    return true;
+	  }
+
+	  @Override
+	  public boolean onOptionsItemSelected(MenuItem item) {
+	    Intent intent = new Intent(
+	        Intent.ACTION_VIEW,
+	        Uri.parse("http://openaphid.github.com/")
+	    );
+	    startActivity(intent);
+
+	    return true;
+	  }
+
+	  @SuppressWarnings("unchecked")
+	  @Override
+	  protected void onListItemClick(ListView l, View v, int position, long id) {
+	    Map<String, Object> map = (Map<String, Object>) l.getItemAtPosition(position);
+	    Intent intent = new Intent(this, (Class<? extends Activity>) map.get("activity"));
+	    startActivity(intent);
+	  }
+
+	  private List<? extends Map<String, ?>> getData() {
+	    List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+	    addItem(data, "MediaAcitivity", MediaAcitivity.class);
+	    addItem(data, "AppListActivity", AppListActivity.class);
+
+	    return data;
+	  }
+
+	  private void addItem(List<Map<String, Object>> data, String title,
+	                       Class<? extends Activity> activityClass) {
+	    Map<String, Object> map = new HashMap<String, Object>();
+	    map.put("title", data.size() + ". " + title);
+	    map.put("activity", activityClass);
+	    data.add(map);
+	  }
 }
